f19726acd3cd3206fbb45ee6669f92e52f86ba87
Robin Medin <robinmedin@telia.com>
2016-03-14 18:48:48 +0100

    fix

--- KronoxScreen/src/publicScreen/PublicScreenGUI.java
+++ KronoxScreen/src/publicScreen/PublicScreenGUI.java
@@ -11,6 +11,7 @@ import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JTextArea;
 import javax.swing.JTextPane;
+import java.awt.Font;
 
 public class PublicScreenGUI extends JFrame {
 
@@ -49,14 +50,33 @@ public class PublicScreenGUI extends JFrame {
 		picWelcomeToNiagara.setBounds(22, 24, 467, 160);
 		contentPane.add(picWelcomeToNiagara);
 		
+		JLabel picMalmoLogo = new JLabel("");
+		picMalmoLogo.setIcon(new ImageIcon(PublicScreenGUI.class.getResource("/Images/Malmo_Hogskola_Logo.png")));
+		picMalmoLogo.setBounds(905, 25, 155, 139);
+		contentPane.add(picMalmoLogo);
+		
 		JPanel panelBlackBox = new JPanel();
 		panelBlackBox.setBackground(Color.BLACK);
 		panelBlackBox.setBounds(0, 184, 1920, 60);
 		contentPane.add(panelBlackBox);
+		panelBlackBox.setLayout(null);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(PublicScreenGUI.class.getResource("/Images/Malmo_Hogskola_Logo.png")));
-		label.setBounds(905, 25, 155, 139);
-		contentPane.add(label);
+		JLabel lblTime = new JLabel("Time");
+		lblTime.setFont(new Font("Futura", Font.PLAIN, 30));
+		lblTime.setForeground(Color.WHITE);
+		lblTime.setBounds(26, 15, 94, 31);
+		panelBlackBox.add(lblTime);
+		
+		JLabel lblCourse = new JLabel("Course");
+		lblCourse.setForeground(Color.WHITE);
+		lblCourse.setFont(new Font("Futura", Font.PLAIN, 30));
+		lblCourse.setBounds(158, 15, 112, 31);
+		panelBlackBox.add(lblCourse);
+		
+		JLabel lblRoom = new JLabel("Room");
+		lblRoom.setForeground(Color.WHITE);
+		lblRoom.setFont(new Font("Futura", Font.PLAIN, 30));
+		lblRoom.setBounds(930, 15, 112, 31);
+		panelBlackBox.add(lblRoom);
 	}
 }
