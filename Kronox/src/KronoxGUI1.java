868daa3b2d42b888388fa305da361896025adced
AntoniaJ <antonia.jungbeck@gmail.com>
2016-03-14 14:28:14 +0100

    igen

--- Kronox/src/KronoxGUI1.java
+++ Kronox/src/KronoxGUI1.java
@@ -30,9 +30,9 @@ public class KronoxGUI1 extends JFrame {
 	 */
 	public KronoxGUI1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1080, 1920);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(5, 5, 1080, 1920));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 	}
